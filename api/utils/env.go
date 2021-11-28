bb414e666636b36c5dc08e6ee65ca0855c2af65e
Yuto Watanabe <yuto.w51942@gmail.com>
2021-11-28 16:09:26 +0900

    rechaptchaのvalidateロジックを書いた

--- api/utils/env.go
+++ api/utils/env.go
@@ -7,3 +7,9 @@ var ONETIME_PASSWORD_ISSUER string = os.Getenv("ONETIME_PASSWORD_ISSUER")
 
 // ワンタイムパスワードのsecret
 var ONETIME_PASSWORD_SECRET []byte = []byte(os.Getenv("ONETIME_PASSWORD_SECRET"))
+
+// デプロイモード
+var DEPLOY_MODE string = os.Getenv("DEPLOY_MODE")
+
+// reCHAPTCHAのsecret
+var RECHAPTCHA_SECRET string = os.Getenv("RECHAPTCHA_SECRET")
---
+++ api/utils/secure/recaptcha.go
@@ -0,0 +1,70 @@
+// Google reCHAPCHAの検証を行う
+//
+// Based on: https://qiita.com/supertaihei02/items/fb15726fd603de7dcefb
+package secure
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/cateiru/cateiru-sso/api/logging"
+	"github.com/cateiru/cateiru-sso/api/utils"
+)
+
+type ReCaptcha struct {
+	ServerName string
+	Secret     string
+}
+
+type RecaptchaResponse struct {
+	Success     bool      `json:"success"`
+	Score       float64   `json:"score"`
+	Action      string    `json:"action"`
+	ChallengeTS time.Time `json:"challenge_ts"`
+	Hostname    string    `json:"hostname"`
+	ErrorCodes  []string  `json:"error-codes"`
+}
+
+func NewReCaptcha() *ReCaptcha {
+	return &ReCaptcha{
+		ServerName: "https://www.google.com/recaptcha/api/siteverify",
+		Secret:     utils.RECHAPTCHA_SECRET,
+	}
+}
+
+// reCHAPTCHAの検証を行い、結果をboolで返す
+func (c *ReCaptcha) Validate(token string, remoteIp string) (bool, error) {
+	result, err := c.ValidateOrder(token, remoteIp)
+	if err != nil {
+		return false, err
+	}
+
+	return result.Success, nil
+}
+
+// reCHAPTCHAの検証を行う
+func (c *ReCaptcha) ValidateOrder(token string, remoteIp string) (*RecaptchaResponse, error) {
+	resp, err := http.PostForm(c.ServerName, url.Values{"secret": {c.Secret}, "remoteip": {remoteIp}, "response": {token}})
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	r := RecaptchaResponse{}
+	if err := json.Unmarshal(body, &r); err != nil {
+		return nil, err
+	}
+
+	logging.Sugar.Debugf(
+		"reCHAPTCHA validate. Token: %s Status: %v, Score: %v, Action: %s, ErrCode: %v",
+		token, r.Success, r.Score, r.Action, r.ErrorCodes)
+
+	return &r, nil
+}
