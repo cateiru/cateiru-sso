afa1ba1b55d75a3b1af727e7b7dbb34eaf30e2d9
Yuto Watanabe <yuto.w51942@gmail.com>
2021-12-05 16:20:45 +0900

    まとめた

--- api/core/create_account/temporary_account.go
+++ api/core/create_account/temporary_account.go
@@ -3,15 +3,22 @@ package createaccount
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/cateiru/cateiru-sso/api/core/common"
 	"github.com/cateiru/cateiru-sso/api/database"
+	"github.com/cateiru/cateiru-sso/api/logging"
 	"github.com/cateiru/cateiru-sso/api/models"
 	"github.com/cateiru/cateiru-sso/api/utils"
+	"github.com/cateiru/cateiru-sso/api/utils/mail"
 	"github.com/cateiru/cateiru-sso/api/utils/net"
 	"github.com/cateiru/go-http-error/httperror/status"
 )
+
+const VERIFY_MAIL_TEMPLATE_PATH = "verify_mail"
 
 type PostForm struct {
 	Mail     string `json:"mail"`
@@ -20,6 +27,11 @@ type PostForm struct {
 
 type Response struct {
 	ClientCheckToken string `json:"client_check_token"`
+}
+
+type VerifyMailTemplate struct {
+	VerifyURL string
+	Mail      string
 }
 
 // 一時的にアカウントを作成します
@@ -89,4 +101,59 @@ func createTemporaryAccount(ctx context.Context, form *PostForm) (string, error)
 	}
 
 	return clientCheckToken, nil
+}
+
+// メール認証を開始します
+//
+// client_check_token(wsを接続するのに使用するトークンを返します)
+func createVerifyMail(ctx context.Context, db *database.Database, user models.UserMailPW) (string, error) {
+	mailToken := utils.CreateID(0)
+	clientCheckToken := utils.CreateID(0)
+
+	mailVerify := &models.MailCertification{
+		MailToken:        mailToken,
+		ClientCheckToken: clientCheckToken,
+		CreateDate:       time.Now(),
+		PeriodMinute:     30,
+
+		OpenNewWindow:  false,
+		Verify:         false,
+		ChangeMailMode: false,
+
+		UserMailPW: user,
+	}
+
+	key := database.CreateNameKey("MailCertification", mailToken)
+
+	if err := db.Put(ctx, key, mailVerify); err != nil {
+		return "", err
+	}
+
+	// send mail
+	// SendGrid APIをテストでは使用しないため、
+	// DEPLOY_MODEがproductionのときのみ送信します
+	if utils.DEPLOY_MODE == "production" {
+		if err := sendVerifyMail(user.Mail, mailToken); err != nil {
+			return "", err
+		}
+	}
+
+	logging.Sugar.Debugf("Send verify email. mail: %s, client check token: %s", user.Mail, clientCheckToken)
+
+	return clientCheckToken, nil
+}
+
+// メールアドレス認証メールを送信する
+func sendVerifyMail(mailAddress string, mailToken string) error {
+	template := VerifyMailTemplate{
+		VerifyURL: fmt.Sprintf("https://%s/create?m=%s", os.Getenv("SITE_DOMAIN"), mailToken),
+		Mail:      mailAddress,
+	}
+
+	mailClient, err := mail.NewMail("", mailAddress, "メールアドレス認証のお知らせ").AddContentsFromTemplate(VERIFY_MAIL_TEMPLATE_PATH, template)
+	if err != nil {
+		return err
+	}
+
+	return mailClient.Send()
 }
