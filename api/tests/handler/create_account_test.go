03d8f9cbfca8e8073267bba3c0575299f9aeaa28
Yuto Watanabe <yuto.w51942@gmail.com>
2021-12-14 19:34:29 +0900

    handlerのテスト追加

--- api/tests/handler/create_account_test.go
+++ api/tests/handler/create_account_test.go
@@ -11,12 +11,14 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"testing"
+	"time"
 
 	createaccount "github.com/cateiru/cateiru-sso/api/core/create_account"
 	"github.com/cateiru/cateiru-sso/api/database"
 	"github.com/cateiru/cateiru-sso/api/handler"
 	"github.com/cateiru/cateiru-sso/api/models"
 	"github.com/cateiru/cateiru-sso/api/utils"
+	"github.com/posener/wstest"
 	"github.com/stretchr/testify/require"
 )
 
@@ -174,6 +176,66 @@ func TestCreateAccount(t *testing.T) {
 
 }
 
+func TestOther(t *testing.T) {
+	app := createAccountServer()
+	server := httptest.NewServer(app)
+	defer server.Close()
+
+	client := &http.Client{}
+
+	resp, err := http.Get(server.URL + "/create")
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+
+	req, err := http.NewRequest("DELETE", server.URL+"/create/verify", nil)
+	require.NoError(t, err)
+	resp, err = client.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+}
+
+func TestObserve(t *testing.T) {
+	t.Setenv("DATASTORE_EMULATOR_HOST", "localhost:18001")
+	t.Setenv("DATASTORE_PROJECT_ID", "project-test")
+
+	ctx := context.Background()
+
+	form := &createaccount.PostForm{
+		Mail:       fmt.Sprintf("%s@example.com", utils.CreateID(4)),
+		Password:   "password",
+		ReCHAPTCHA: "",
+	}
+	ip := "192.168.1.1"
+
+	clientCheckToken, err := createaccount.CreateTemporaryAccount(ctx, form, ip)
+	require.NoError(t, err)
+
+	////
+
+	server := createAccountServer()
+
+	d := wstest.NewDialer(server)
+
+	c, resp, err := d.Dial(fmt.Sprintf("ws://whatever/create/verify?cct=%s", clientCheckToken), nil)
+	require.NoError(t, err)
+	got, want := resp.StatusCode, http.StatusSwitchingProtocols
+	require.Equal(t, got, want)
+
+	go verifyMail(ctx, t, clientCheckToken)
+
+	// 受信待機
+	var respm bool
+	err = c.ReadJSON(&respm)
+	require.NoError(t, err)
+
+	// 返ってくる = メール認証が完了したためclient側からwsをcloseする
+	err = c.Close()
+	require.NoError(t, err)
+
+	// response messageは`true`が返る
+	require.True(t, respm)
+}
+
 // clientCheckTokenからmailTokenを取得する
 func getMailToken(ctx context.Context, clientCheckToken string) (string, error) {
 	db, err := database.NewDatabase(ctx)
@@ -202,4 +264,23 @@ func respToJson(resp *http.Response, obj interface{}) error {
 	buf.ReadFrom(resp.Body)
 
 	return json.Unmarshal(buf.Bytes(), obj)
+}
+
+func verifyMail(ctx context.Context, t *testing.T, clientCheckToken string) {
+	// 3秒間待機する: WSで待機するため
+	time.Sleep(3 * time.Second)
+
+	db, err := database.NewDatabase(ctx)
+	require.NoError(t, err)
+	defer db.Close()
+
+	cert, err := models.GetMailCertificationByCheckToken(ctx, db, clientCheckToken)
+	require.NoError(t, err)
+	require.NotNil(t, cert)
+
+	t.Logf("verify mailToken: %s", cert.MailToken)
+	cert.Verify = true
+
+	err = cert.Add(ctx, db)
+	require.NoError(t, err)
 }
