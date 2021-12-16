49664cb1adaa42e28cbb76ff827fd9073f38ca18
Yuto Watanabe <yuto.w51942@gmail.com>
2021-12-16 21:50:27 +0900

    meの認証が失敗したときのテストを追加

--- api/tests/handler/me_test.go
+++ api/tests/handler/me_test.go
@@ -71,3 +71,20 @@ func TestMe(t *testing.T) {
 	require.Equal(t, userInfo.UserId.UserId, dummy.UserID)
 	require.Equal(t, userInfo.Mail, dummy.Mail)
 }
+
+func TestMeNotVerify(t *testing.T) {
+	t.Setenv("DATASTORE_EMULATOR_HOST", "localhost:18001")
+	t.Setenv("DATASTORE_PROJECT_ID", "project-test")
+
+	app := meServer()
+	server := httptest.NewServer(app)
+	defer server.Close()
+
+	jar, err := cookiejar.New(nil)
+	require.NoError(t, err, "cookiejarでエラー")
+	client := &http.Client{Jar: jar}
+
+	resp, err := client.Get(server.URL + "/")
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 403, "認証情報がないので何も返さない")
+}
