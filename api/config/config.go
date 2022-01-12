b3648a36a9e51ba400149c920e6779d1d5a450c9
Yuto Watanabe <yuto.w51942@gmail.com>
2022-01-12 17:48:03 +0900

    configを追加

---
+++ api/config/config.go
@@ -0,0 +1,106 @@
+package config
+
+import (
+	"os"
+	"testing"
+)
+
+type Config struct {
+	DeployMode string
+
+	Port    string
+	Address string
+
+	AdminMail     string
+	AdminPassword string
+
+	CookieDomain string
+	SiteDomain   string
+	APIDomain    string
+
+	Issuer string
+
+	ReChaptchaSecret string
+
+	MailgunAPIKey     string
+	MailFromDomain    string
+	SenderMailAddress string
+
+	DatastoreParentKey string
+}
+
+var Defs Config
+
+func Init() {
+	Defs = Config{
+		DeployMode: getDeployMode(),
+
+		Port:    getPort(),
+		Address: "0.0.0.0",
+
+		AdminMail:     os.Getenv("ADMIN_MAIL"),
+		AdminPassword: os.Getenv("ADMIN_PASSWORD"),
+
+		CookieDomain: os.Getenv("COOKIE_DOMAIN"),
+		SiteDomain:   os.Getenv("SITE_DOMAIN"),
+		APIDomain:    os.Getenv("API_DOMAIN"),
+
+		Issuer: getIssuer(),
+
+		ReChaptchaSecret: os.Getenv("RECAPTCHA_SECRET"),
+
+		MailgunAPIKey:     os.Getenv("MAILGUN_API_KEY"),
+		MailFromDomain:    os.Getenv("MAIL_FROM_DOMAIL"),
+		SenderMailAddress: os.Getenv("SENDER_MAIL_ADDRESS"),
+
+		DatastoreParentKey: getDatastoreParentKey(),
+	}
+}
+
+func TestInit(t *testing.T) {
+	Init()
+
+	t.Setenv("DATASTORE_EMULATOR_HOST", "localhost:18001")
+	t.Setenv("DATASTORE_PROJECT_ID", "project-test")
+}
+
+// envが設定されていない場合は、`develop`を指定します
+func getDeployMode() string {
+	deployMode := os.Getenv("DEPLOY_MODE")
+
+	if len(deployMode) == 0 {
+		deployMode = "develop"
+	}
+
+	return deployMode
+}
+
+// envが設定されていない場合、3000を使用します
+func getPort() string {
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	return port
+}
+
+func getIssuer() string {
+	issuer := os.Getenv("ISSUER")
+
+	if len(issuer) == 0 {
+		issuer = "TestIssuer"
+	}
+
+	return issuer
+}
+
+func getDatastoreParentKey() string {
+	parentKey := os.Getenv("DATASTORE_PARENT_KEY")
+	if len(parentKey) == 0 {
+		parentKey = "cateiru-sso"
+	}
+
+	return parentKey
+}
