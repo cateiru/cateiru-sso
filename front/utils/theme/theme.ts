6b62e8c582885155fc07afac2787a34ef1220ec3
Yuto Watanabe <yuto.w51942@gmail.com>
2022-02-02 19:49:11 +0900

    スクロールバーのデザインかっこよくした

--- front/utils/theme/theme.ts
+++ front/utils/theme/theme.ts
@@ -1,4 +1,4 @@
-import {extendTheme} from '@chakra-ui/react';
+import {extendTheme, } from '@chakra-ui/react';
 import {StepsStyleConfig as Steps} from 'chakra-ui-steps';
 
 const theme = extendTheme({
@@ -15,6 +15,23 @@ const theme = extendTheme({
       },
     },
     Steps,
+  },
+  styles: {
+    global: (props: any) => ({
+      // Chrome
+      '&::-webkit-scrollbar': {
+        width: '10px',
+      },
+      '&::-webkit-scrollbar-thumb': {
+        backgroundColor: props.colorMode === 'dark' ? 'gray.600' : 'gray.300',
+        borderRadius: '100px',
+      },
+      // FireFox
+      html: {
+        scrollbarWidth: 'thin',
+        scrollbarColor: props.colorMode === 'dark' ? 'gray.600' : 'gray.300',
+      }
+    }),
   },
 });
 
