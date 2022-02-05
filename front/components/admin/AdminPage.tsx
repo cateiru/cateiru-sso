7225d14a75d3f6da0888663e8732a654a8e9a4a9
Yuto Watanabe <yuto.w51942@gmail.com>
2022-02-06 01:42:37 +0900

    adminページ作った

---
+++ front/components/admin/AdminPage.tsx
@@ -0,0 +1,92 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Center,
+  Avatar,
+  Link,
+  useToast,
+  Heading,
+  Box,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import React from 'react';
+import {getAllUsers} from '../../utils/api/admin';
+import {UserInfo} from '../../utils/state/types';
+
+const AdminPage = () => {
+  const [users, setUsers] = React.useState<UserInfo[]>([]);
+  const toast = useToast();
+
+  React.useEffect(() => {
+    const f = async () => {
+      try {
+        const users = await getAllUsers();
+        setUsers(users);
+      } catch (error) {
+        if (error instanceof Error) {
+          toast({
+            title: error.message,
+            status: 'error',
+            isClosable: true,
+            duration: 9000,
+          });
+        }
+      }
+    };
+
+    f();
+  }, []);
+
+  const user = (user: UserInfo) => {
+    return (
+      <Tr cursor="pointer" key={user.user_id}>
+        <Td>
+          <Center>
+            <Avatar src={user.avatar_url} size="sm" />
+          </Center>
+        </Td>
+        <Td textAlign="center">
+          <NextLink href={`/admin/user?id=${user.user_id}`} passHref>
+            <Link>{user.user_name}</Link>
+          </NextLink>
+        </Td>
+        <Td textAlign="center">{user.mail}</Td>
+        <Td textAlign="center">{`${user.last_name} ${user.first_name}`}</Td>
+      </Tr>
+    );
+  };
+
+  return (
+    <Center>
+      <Box width={{base: '100%', lg: '1000px'}} mt="2rem">
+        <Heading textAlign="center">
+          すべてのユーザ（全{user.length}人）
+        </Heading>
+        <Box mx=".5rem" overflowX={{base: 'auto', lg: 'visible'}} mt="1rem">
+          <Table
+            variant="striped"
+            minWidth="calc(1000px - 1rem)"
+            size="lg"
+            alignItems="center"
+          >
+            <Thead>
+              <Tr>
+                <Th></Th>
+                <Th textAlign="center">ユーザ名</Th>
+                <Th textAlign="center">メールアドレス</Th>
+                <Th textAlign="center">本名</Th>
+              </Tr>
+            </Thead>
+            <Tbody>{users.map(value => user(value))}</Tbody>
+          </Table>
+        </Box>
+      </Box>
+    </Center>
+  );
+};
+
+export default AdminPage;
---
+++ front/pages/admin.tsx
@@ -0,0 +1,14 @@
+import AdminPage from '../components/admin/AdminPage';
+import Require from '../components/common/Require';
+import Title from '../components/common/Title';
+
+const Admin = () => {
+  return (
+    <Require isLogin={true} path="/hello" role="admin">
+      <Title title="Admin | CateiruSSO" />
+      <AdminPage />
+    </Require>
+  );
+};
+
+export default Admin;
