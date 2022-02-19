13864e93222b3f37bd3f70155814f46852d9709f
Yuto Watanabe <yuto.w51942@gmail.com>
2022-02-19 18:44:46 +0900

    テスト追加

--- api/tests/handler/login_test.go
+++ api/tests/handler/login_test.go
@@ -1,6 +1,7 @@
 package handler_test
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"net/http"
@@ -96,4 +97,111 @@ func TestLoginOTP(t *testing.T) {
 	// ---
 
 	s.FindCookies(t, []string{"session-token", "refresh-token"})
+}
+
+func TestFailedPWNoOTP(t *testing.T) {
+	config.TestInit(t)
+
+	ctx := context.Background()
+
+	db, err := database.NewDatabase(ctx)
+	require.NoError(t, err)
+	defer db.Close()
+
+	dummy := tools.NewDummyUser() // OTPは設定しない
+	_, err = dummy.AddUserCert(ctx, db)
+	require.NoError(t, err)
+	_, err = dummy.AddUserInfo(ctx, db)
+	require.NoError(t, err)
+
+	s := tools.NewTestServer(t, loginServer(), true)
+
+	send := login.RequestFrom{
+		Mail:     dummy.Mail,
+		Password: "hoge",
+	}
+
+	reqForm, err := json.Marshal(send)
+	require.NoError(t, err)
+
+	resp, err := s.Client.Post(s.Server.URL+"/login", "application/json", bytes.NewBuffer(reqForm))
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 400)
+}
+
+func TestFailedPWOTP(t *testing.T) {
+	config.TestInit(t)
+
+	ctx := context.Background()
+
+	db, err := database.NewDatabase(ctx)
+	require.NoError(t, err)
+	defer db.Close()
+
+	dummy, err := tools.NewDummyUser().NewOTP()
+	require.NoError(t, err)
+	_, err = dummy.AddUserCert(ctx, db)
+	require.NoError(t, err)
+	_, err = dummy.AddUserInfo(ctx, db)
+	require.NoError(t, err)
+
+	s := tools.NewTestServer(t, loginServer(), true)
+
+	send := login.RequestFrom{
+		Mail:     dummy.Mail,
+		Password: "hoge",
+	}
+
+	reqForm, err := json.Marshal(send)
+	require.NoError(t, err)
+
+	resp, err := s.Client.Post(s.Server.URL+"/login", "application/json", bytes.NewBuffer(reqForm))
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 400)
+}
+
+func TestFailedOTP(t *testing.T) {
+	config.TestInit(t)
+
+	ctx := context.Background()
+
+	db, err := database.NewDatabase(ctx)
+	require.NoError(t, err)
+	defer db.Close()
+
+	dummy, err := tools.NewDummyUser().NewOTP()
+	require.NoError(t, err)
+	_, err = dummy.AddUserCert(ctx, db)
+	require.NoError(t, err)
+	_, err = dummy.AddUserInfo(ctx, db)
+	require.NoError(t, err)
+
+	s := tools.NewTestServer(t, loginServer(), true)
+
+	send := login.RequestFrom{
+		Mail:     dummy.Mail,
+		Password: "password",
+	}
+
+	resp := s.Post(t, "/login", send)
+
+	var response login.Response
+	err = json.Unmarshal(tools.ConvertByteResp(resp), &response)
+	require.NoError(t, err)
+
+	require.True(t, response.IsOTP)
+
+	// ---
+
+	otpSend := login.OTPRequest{
+		Passcode: "123456",
+		OtpToken: response.OTPToken,
+	}
+
+	reqForm, err := json.Marshal(otpSend)
+	require.NoError(t, err)
+
+	resp, err = s.Client.Post(s.Server.URL+"/login/otp", "application/json", bytes.NewBuffer(reqForm))
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 400)
 }
