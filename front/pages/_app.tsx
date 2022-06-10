7c714bfce836426a8f64f8131c71a1cd3095774b
Yuto Watanabe <yuto.w51942@gmail.com>
2022-06-10 21:51:46 +0900

    font変更

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -6,12 +6,13 @@ import {useEffect} from 'react';
 import React from 'react';
 import {GoogleReCaptchaProvider} from 'react-google-recaptcha-v3';
 import {RecoilRoot} from 'recoil';
-import Font from '../components/common/Font';
 import Load from '../components/common/Load';
 import Me from '../components/common/Me';
 import Page from '../components/common/Page';
 import {GA_TRACKING_ID, pageview} from '../utils/ga/gtag';
 import theme from '../utils/theme/theme';
+
+import '@fontsource/noto-sans-jp';
 import 'nprogress/nprogress.css';
 
 nprogress.configure({showSpinner: false, speed: 400, minimum: 0.25});
@@ -65,7 +66,6 @@ const App = ({Component, pageProps}: AppProps) => {
           language="ja"
           scriptProps={scriptProps}
         >
-          <Font />
           <Load />
           <Me>
             <Page>
