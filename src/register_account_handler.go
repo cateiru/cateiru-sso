59de68578c7885647d64eaf885b984443e6790dd
Yuto Watanabe <yuto.w51942@gmail.com>
2023-02-15 00:31:41 +0900

    WIP: メール認証handler書いた

--- src/register_account_handler.go
+++ src/register_account_handler.go
@@ -2,15 +2,27 @@ package src
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/cateiru/cateiru-sso/src/lib"
+	"github.com/cateiru/cateiru-sso/src/models"
 	"github.com/labstack/echo/v4"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 	"go.uber.org/zap"
 )
+
+type RegisterEmailVerify struct {
+	Code     string
+	Email    string
+	Time     time.Time
+	UserData *UserData
+}
 
 // 最初にメールアドレス宛に確認コードを送信する
 // アカウント作成フローの一番はじめ
 func (h *Handler) SendEmailVerifyHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+
 	email := c.FormValue("email")
 	recaptcha := c.FormValue("recaptcha")
 	ip := c.RealIP()
@@ -32,14 +44,82 @@ func (h *Handler) SendEmailVerifyHandler(c echo.Context) error {
 		}
 	}
 
+	// セッションテーブルにEmailが存在しているか
+	// 有効期限が切れるまで同じメールアドレスでセッションを作れないようにする
+	// スパム防止のため
+	existsEmailInRegisterSession, err := models.RegisterOtpSessions(
+		models.RegisterSessionWhere.Email.EQ(email),
+	).Exists(ctx, h.DB)
+	if err != nil {
+		return err
+	}
+	if existsEmailInRegisterSession {
+		return NewHTTPUniqueError(http.StatusBadRequest, ErrSessionExists, "session exists")
+	}
+
+	// すでに登録されているメールアドレスの場合は登録できない（Emailがユニークなため）
+	exitsEmailInUser, err := models.Users(
+		models.UserWhere.Email.EQ(email),
+	).Exists(ctx, h.DB)
+	if err != nil {
+		return err
+	}
+	if exitsEmailInUser {
+		return NewHTTPUniqueError(http.StatusBadRequest, ErrImpossibleRegisterAccount, "impossible register")
+	}
+
 	userData, err := ParseUA(c.Request())
 	if err != nil {
 		return err
 	}
+	session, err := lib.RandomStr(31)
+	if err != nil {
+		return err
+	}
+	code, err := lib.RandomNumber(6)
+	if err != nil {
+		return err
+	}
 
-	// メールを送信するのでログを出す
+	// セッションを作成する
+	// email_verifiedはデフォルトfalse
+	// retry_countはデフォルト0
+	sessionDB := models.RegisterSession{
+		ID:         session,
+		Email:      email,
+		VerifyCode: code,
+
+		Period: time.Now().Add(h.C.RegisterSessionPeriod),
+	}
+	if err := sessionDB.Insert(ctx, h.DB, boil.Infer()); err != nil {
+		return err
+	}
+
+	// 対象のメールアドレスにメールを送信する
+	r := RegisterEmailVerify{
+		Code:     code,
+		Email:    email,
+		Time:     time.Now(),
+		UserData: userData,
+	}
+	m := &lib.MailBody{
+		EmailAddress:      email,
+		Subject:           "メールアドレスの登録確認",
+		Data:              r,
+		PlainTextFileName: "register.gtpl",
+		HTMLTextFileName:  "register.html",
+	}
+	msg, id, err := h.Sender.Send(m)
+	if err != nil {
+		// TODO: セッションテーブル削除するなど丁寧にやりたい
+		return err
+	}
+
+	// メールを送信したのでログを出す
 	L.Info("mail",
 		zap.String("Email", email),
+		zap.String("MailGunMessage", msg),
+		zap.String("MailGunID", id),
 		zap.String("IP", ip),
 		zap.String("Device", userData.Device),
 		zap.String("Browser", userData.Browser),
