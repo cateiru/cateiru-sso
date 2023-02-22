51447f79f8fa2b6028ff857a21e6dfd717e6561b
Yuto Watanabe <yuto.w51942@gmail.com>
2023-02-23 03:03:34 +0900

    route追加

--- src/routes.go
+++ src/routes.go
@@ -9,4 +9,5 @@ func Routes(e *echo.Echo, h *Handler) {
 	e.POST("/v2/register/email/send", h.SendEmailVerifyHandler)
 	e.POST("/v2/register/email/resend", h.ReSendVerifyEmailHandler)
 	e.POST("/v2/register/email/verify", h.RegisterVerifyEmailHandler)
+	e.POST("/v2/register/begin_webauthn", h.RegisterBeginWebAuthn)
 }
