574d371677fc8124527454054aaf28920ef23553
Yuto Watanabe <yuto.w51942@gmail.com>
2023-05-12 22:19:05 +0900

    Duplicate atom keyをignoreする

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const withInterceptStdout = require('next-intercept-stdout');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-};
+module.exports = withInterceptStdout(
+  {
+    reactStrictMode: true,
+  },
+  text => (text.includes('Duplicate atom key') ? '' : text)
+);
