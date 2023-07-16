9163cc4aef971f5d9c259b5f3bde71c102916409
Yuto Watanabe <yuto.w51942@gmail.com>
2023-07-16 19:13:05 +0900

    fix

--- components/Common/Chakra/Avatar.tsx
+++ components/Common/Chakra/Avatar.tsx
@@ -1,10 +1,18 @@
-import {Avatar as ChakraAvatar, type AvatarProps} from '@chakra-ui/react';
+import {
+  Avatar as ChakraAvatar,
+  type AvatarProps,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import React from 'react';
 
 export const Avatar: React.FC<AvatarProps> = props => {
+  const shadowColor = useColorModeValue('#242838', '#000');
+
   if (props.src === '') {
     return <ChakraAvatar {...props} key={`no-image-avatar-${Math.random()}`} />;
   }
 
-  return <ChakraAvatar {...props} />;
+  return (
+    <ChakraAvatar {...props} boxShadow={`0px 0px 7px -2px ${shadowColor}`} />
+  );
 };
