a2ac3f76b3c888f7d61f60e2dd6f4a73e7510a34
Yuto Watanabe <yuto.w51942@gmail.com>
2023-07-23 00:46:19 +0900

    fix

--- app/clients/layout.tsx
+++ app/clients/layout.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {ClientsListWrapper} from '../../components/Client/ClientsListWrapper';
+import {Session} from '../../components/Common/Session';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<Props> = ({children}) => {
-  return <ClientsListWrapper>{children}</ClientsListWrapper>;
+  return (
+    <Session>
+      <ClientsListWrapper>{children}</ClientsListWrapper>
+    </Session>
+  );
 };
 
 export default Layout;
