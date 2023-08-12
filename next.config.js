db19807675dbfb2c31b1efdc795192eb41cacdc5
Yuto Watanabe <yuto.w51942@gmail.com>
2023-08-12 19:48:13 +0900

    cache controlヘッダーを追加

--- next.config.js
+++ next.config.js
@@ -6,7 +6,20 @@ module.exports = withInterceptStdout(
     reactStrictMode: true,
     output: 'standalone',
     experimental: {
-        scrollRestoration: true,
+      scrollRestoration: true,
+    },
+    async headers() {
+      return [
+        {
+          source: '*',
+          headers: [
+            {
+              key: 'cache-control',
+              value: 'max-age=600, stale-while-revalidate=1800', // 10分キャッシュし、30分間は古いキャッシュを返す
+            },
+          ],
+        },
+      ];
     },
   },
   text => (text.includes('Duplicate atom key') ? '' : text)
