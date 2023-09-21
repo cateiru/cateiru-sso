0a851831ee614a4a106582e0b22abf516658a6c1
cateiru <yuto.w51942@gmail.com>
2023-09-22 00:15:14 +0900

    Authentication Requestのvalidate関数作った

--- src/lib/validate_oidc_string.go
+++ src/lib/validate_oidc_string.go
@@ -1,11 +1,57 @@
 package lib
 
+import (
+	"strconv"
+)
+
 // サポートしているOIDCのスコープ
 var AllowScopes = []string{
 	"openid",
 	"profile",
 	"email",
 }
+
+type ResponseType string
+
+const (
+	// 不正な値の場合
+	ResponseTypeInvalid ResponseType = ""
+
+	// Authorization Code Flow
+	ResponseTypeAuthorizationCode ResponseType = "code"
+
+	// Implicit Flow
+	ResponseTypeImplicit ResponseType = "id_token"
+
+	// Hybrid Flow
+	ResponseTypeHybrid ResponseType = "code id_token"
+)
+
+type ResponseMode string
+
+const (
+	ResponseModeQuery    ResponseMode = "query"
+	ResponseModeFragment ResponseMode = "fragment"
+	ResponseModeFormPost ResponseMode = "form_post"
+)
+
+type Display string
+
+const (
+	DisplayPage  Display = "page"
+	DisplayPopup Display = "popup"
+	DisplayTouch Display = "touch"
+	DisplayWap   Display = "wap"
+)
+
+type Prompt string
+
+const (
+	PromptNone          Prompt = "none"
+	PromptLogin         Prompt = "login"
+	PromptConsent       Prompt = "consent"
+	PromptSelectAccount Prompt = "select_account"
+)
 
 func ValidateScope(s string) bool {
 	for _, allow := range AllowScopes {
@@ -14,4 +60,73 @@ func ValidateScope(s string) bool {
 		}
 	}
 	return false
+}
+
+func ValidateResponseType(s string) ResponseType {
+	switch s {
+	case "code":
+		return ResponseTypeAuthorizationCode
+	case "id_token", "id_token token":
+		// id_token, id_token token は同じ扱いなので1つにまとめる
+		return ResponseTypeImplicit
+	case "code id_token", "code token", "code id_token token":
+		return ResponseTypeHybrid
+	}
+
+	return ResponseTypeInvalid
+}
+
+// response_mode のバリデーション
+// デフォルトは query
+// https://openid.net/specs/oauth-v2-multiple-response-types-1_0.html
+func ValidateResponseMode(s string) ResponseMode {
+	switch s {
+	case "query":
+		return ResponseModeQuery
+	case "fragment":
+		return ResponseModeFragment
+	case "form_post":
+		return ResponseModeFormPost
+	}
+
+	return ResponseModeQuery
+}
+
+// 一旦page決め打ち
+func ValidateDisplay(s string) Display {
+	return DisplayPage
+}
+
+func ValidatePrompt(s string) Prompt {
+	switch s {
+	case "none":
+		return PromptNone
+	case "login":
+		return PromptLogin
+	case "consent":
+		return PromptConsent
+	case "select_account":
+		return PromptSelectAccount
+	}
+
+	return PromptNone
+}
+
+// max-age
+// OP によって明示的に認証されてからの経過時間の最大許容値 (秒)。指定しない場合や不正な値の場合は0
+func ValidateMaxAge(s string) int {
+	if s == "" {
+		return 0
+	}
+
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+
+	if n <= 0 {
+		return 0
+	}
+
+	return n
 }
--- src/lib/validate_oidc_string_test.go
+++ src/lib/validate_oidc_string_test.go
@@ -32,3 +32,96 @@ func TestValidateScope(t *testing.T) {
 		}
 	})
 }
+
+func TestValidateResponseType(t *testing.T) {
+	t.Run("成功: Authorization Code Flow", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseType("code"), lib.ResponseTypeAuthorizationCode)
+	})
+
+	t.Run("成功: Implicit Flow", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseType("id_token"), lib.ResponseTypeImplicit)
+		require.Equal(t, lib.ValidateResponseType("id_token token"), lib.ResponseTypeImplicit)
+	})
+
+	t.Run("成功: Hybrid Flow", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseType("code id_token"), lib.ResponseTypeHybrid)
+		require.Equal(t, lib.ValidateResponseType("code token"), lib.ResponseTypeHybrid)
+		require.Equal(t, lib.ValidateResponseType("code id_token token"), lib.ResponseTypeHybrid)
+	})
+
+	t.Run("失敗: 値が空", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseType(""), lib.ResponseTypeInvalid)
+	})
+
+	t.Run("失敗: 値が不正", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseType("aaaa"), lib.ResponseTypeInvalid)
+	})
+}
+
+func TestValidateResponseMode(t *testing.T) {
+	t.Run("成功: query", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseMode("query"), lib.ResponseModeQuery)
+	})
+
+	t.Run("成功: fragment", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseMode("fragment"), lib.ResponseModeFragment)
+	})
+
+	t.Run("成功: form_post", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseMode("form_post"), lib.ResponseModeFormPost)
+	})
+
+	t.Run("値が空の場合はqueryになる", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseMode(""), lib.ResponseModeQuery)
+	})
+
+	t.Run("値が不正の場合はqueryになる", func(t *testing.T) {
+		require.Equal(t, lib.ValidateResponseMode("aaaa"), lib.ResponseModeQuery)
+	})
+}
+
+func TestValidateDisplay(t *testing.T) {
+	require.Equal(t, lib.ValidateResponseMode("aaa"), lib.DisplayPage, "決め打ちでpageになる")
+}
+
+func TestValidatePrompt(t *testing.T) {
+	t.Run("成功: none", func(t *testing.T) {
+		require.Equal(t, lib.ValidatePrompt("none"), lib.PromptNone)
+	})
+
+	t.Run("成功: login", func(t *testing.T) {
+		require.Equal(t, lib.ValidatePrompt("login"), lib.PromptLogin)
+	})
+
+	t.Run("成功: consent", func(t *testing.T) {
+		require.Equal(t, lib.ValidatePrompt("consent"), lib.PromptConsent)
+	})
+
+	t.Run("成功: select_account", func(t *testing.T) {
+		require.Equal(t, lib.ValidatePrompt("select_account"), lib.PromptSelectAccount)
+	})
+
+	t.Run("値が空の場合はnoneになる", func(t *testing.T) {
+		require.Equal(t, lib.ValidatePrompt(""), lib.PromptNone)
+	})
+
+	t.Run("値が不正の場合はnoneになる", func(t *testing.T) {
+		require.Equal(t, lib.ValidatePrompt("aaaa"), lib.PromptNone)
+	})
+}
+
+func TestValidateMaxAge(t *testing.T) {
+
+	t.Run("成功: max-ageを指定している", func(t *testing.T) {
+		require.Equal(t, lib.ValidateMaxAge("100"), 100)
+	})
+
+	t.Run("空の場合は0になる", func(t *testing.T) {
+		require.Equal(t, lib.ValidateMaxAge(""), 0)
+	})
+
+	t.Run("負の値の場合は0になる", func(t *testing.T) {
+		require.Equal(t, lib.ValidateMaxAge("-1"), 0)
+	})
+
+}
