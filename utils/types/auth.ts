6cf52789c6b4eb68a744a62aba390866297386cd
cateiru <yuto.w51942@gmail.com>
2023-10-09 21:31:51 +0900

    型更新

--- utils/types/auth.ts
+++ utils/types/auth.ts
@@ -8,10 +8,14 @@ export const publicAuthenticationRequestSchema = z.object({
 
   org_name: z.string().nullable(),
   org_image: z.string().url().nullable(),
+  org_member_only: z.boolean(),
 
   scopes: z.array(z.string()),
   redirect_uri: z.string().url(),
   response_type: z.string(),
+
+  register_user_name: z.string(),
+  register_user_image: z.string().nullable(),
 });
 export type PublicAuthenticationRequest = z.infer<
   typeof publicAuthenticationRequestSchema
