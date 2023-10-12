f48277952b69364b1f3a2efd1ac1aabd7b5fdb97
cateiru <yuto.w51942@gmail.com>
2023-10-12 22:56:40 +0900

    user_nameテーブル追加

---
+++ db/migrations/20231012135551_user_name.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `user_name` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+
+    `user_name` VARCHAR(15) NOT NULL,
+
+    `user_id` VARCHAR(32) NOT NULL,
+
+    `period` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    -- 管理用
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+    INDEX `user_name_user_name_period` (`user_name`, `period`),
+    INDEX `user_name_user_name_id` (`user_name`, `user_id`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ENGINE=InnoDB;
--- db/schema.sql
+++ db/schema.sql
@@ -114,6 +114,25 @@ CREATE TABLE `staff` (
     PRIMARY KEY (`user_id`)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ENGINE=InnoDB;
 
+-- ユーザー名保存
+CREATE TABLE `user_name` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+
+    `user_name` VARCHAR(15) NOT NULL,
+
+    `user_id` VARCHAR(32) NOT NULL,
+
+    `period` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    -- 管理用
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+    INDEX `user_name_user_name_period` (`user_name`, `period`),
+    INDEX `user_name_user_name_id` (`user_name`, `user_id`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ENGINE=InnoDB;
+
 -- WebAuthnを保存するテーブル
 CREATE TABLE `webauthn` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
