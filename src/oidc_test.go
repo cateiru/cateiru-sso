b0eebed2531b8dea7622e94239d6a2a6c0cc48ce
cateiru <yuto.w51942@gmail.com>
2023-11-20 22:59:11 +0900

    テスト追加

--- src/oidc_test.go
+++ src/oidc_test.go
@@ -2,9 +2,11 @@ package src_test
 
 import (
 	"context"
+	"net/url"
 	"testing"
 
 	"github.com/cateiru/cateiru-sso/src"
+	"github.com/cateiru/cateiru-sso/src/lib"
 	"github.com/cateiru/cateiru-sso/src/models"
 	"github.com/cateiru/go-http-easy-test/v2/easy"
 	"github.com/stretchr/testify/require"
@@ -169,12 +171,278 @@ func TestOIDCRequireHandler(t *testing.T) {
 	})
 }
 
-// TODO: テスト
 func TestOIDCLoginHandler(t *testing.T) {
+	ctx := context.Background()
+	h := NewTestHandler(t)
 
+	SessionTest(t, h.OIDCLoginHandler, func(ctx context.Context, u *models.User) *easy.MockHandler {
+		clientId, _ := RegisterClient(t, ctx, u, "openid", "profile")
+
+		r := models.ClientRedirect{
+			ClientID: clientId,
+			URL:      "https://example.test",
+			Host:     "example.test",
+		}
+		err := r.Insert(ctx, DB, boil.Infer())
+		require.NoError(t, err)
+
+		form := easy.NewMultipart()
+
+		state, err := lib.RandomStr(10)
+		require.NoError(t, err)
+
+		form.Insert("scope", "openid profile email")
+		form.Insert("response_type", "code")
+		form.Insert("client_id", clientId)
+		form.Insert("redirect_uri", "https://example.test")
+		form.Insert("state", state)
+		form.Insert("response_mode", "query")
+		form.Insert("nonce", "nonce_test")
+		form.Insert("display", "page")
+		form.Insert("prompt", "login consent")
+		form.Insert("max_age", "3600")
+		form.Insert("ui_locales", "ja_JP")
+		form.Insert("id_token_hint", "id_token_hint_test")
+		form.Insert("login_hint", "login_hint_test")
+		form.Insert("acr_values", "acr_values_test")
+
+		m, err := easy.NewFormData("/", "POST", form)
+		require.NoError(t, err)
+		return m
+	})
+
+	t.Run("成功: submitできる", func(t *testing.T) {
+		email := RandomEmail(t)
+		u := RegisterUser(t, ctx, email)
+		clientId, _ := RegisterClient(t, ctx, &u, "openid", "profile")
+
+		r := models.ClientRedirect{
+			ClientID: clientId,
+			URL:      "https://example.test",
+			Host:     "example.test",
+		}
+		err := r.Insert(ctx, DB, boil.Infer())
+		require.NoError(t, err)
+
+		cookies := RegisterSession(t, ctx, &u)
+
+		form := easy.NewMultipart()
+
+		state, err := lib.RandomStr(10)
+		require.NoError(t, err)
+
+		form.Insert("scope", "openid profile email")
+		form.Insert("response_type", "code")
+		form.Insert("client_id", clientId)
+		form.Insert("redirect_uri", "https://example.test")
+		form.Insert("state", state)
+		form.Insert("response_mode", "query")
+		form.Insert("nonce", "nonce_test")
+		form.Insert("display", "page")
+		form.Insert("prompt", "login consent")
+		form.Insert("max_age", "3600")
+		form.Insert("ui_locales", "ja_JP")
+		form.Insert("id_token_hint", "id_token_hint_test")
+		form.Insert("login_hint", "login_hint_test")
+		form.Insert("acr_values", "acr_values_test")
+
+		m, err := easy.NewFormData("/", "POST", form)
+		require.NoError(t, err)
+		m.Cookie(cookies)
+
+		c := m.Echo()
+
+		err = h.OIDCLoginHandler(c)
+		require.NoError(t, err)
+
+		response := src.OauthResponse{}
+		require.NoError(t, m.Json(&response))
+
+		require.NotEmpty(t, response.RedirectUrl)
+
+		redirectUrl, err := url.Parse(response.RedirectUrl)
+		require.NoError(t, err)
+
+		require.Equal(t, redirectUrl.Host, "example.test")
+		require.Equal(t, redirectUrl.Query().Get("state"), state, "stateが一致する")
+
+		code := redirectUrl.Query().Get("code")
+		require.NotEmpty(t, code, "codeがある")
+
+		existSession, err := models.OauthSessions(
+			models.OauthSessionWhere.Code.EQ(code),
+		).Exists(ctx, DB)
+		require.NoError(t, err)
+
+		require.True(t, existSession, "DBにセッションが存在している")
+
+		operationHistory, err := models.OperationHistories(
+			models.OperationHistoryWhere.UserID.EQ(u.ID),
+		).One(ctx, DB)
+		require.NoError(t, err)
+		require.Equal(t, operationHistory.Identifier, int8(1), "ログが保存されている")
+	})
+
+	t.Run("失敗: ユーザーチェック不可", func(t *testing.T) {
+		email := RandomEmail(t)
+		u := RegisterUser(t, ctx, email)
+		clientId, _ := RegisterClient(t, ctx, &u, "openid", "profile")
+
+		// Allow Ruleを設定してユーザーを弾く
+		client, err := models.Clients(
+			models.ClientWhere.ClientID.EQ(clientId),
+		).One(ctx, DB)
+		require.NoError(t, err)
+
+		client.IsAllow = true
+
+		_, err = client.Update(ctx, DB, boil.Infer())
+		require.NoError(t, err)
+
+		r := models.ClientRedirect{
+			ClientID: clientId,
+			URL:      "https://example.test",
+			Host:     "example.test",
+		}
+		err = r.Insert(ctx, DB, boil.Infer())
+		require.NoError(t, err)
+
+		// ルール作成
+		allowRule := models.ClientAllowRule{
+			ClientID:    clientId,
+			EmailDomain: null.NewString("nya.test", true),
+		}
+		require.NoError(t, allowRule.Insert(ctx, DB, boil.Infer()))
+
+		cookies := RegisterSession(t, ctx, &u)
+
+		form := easy.NewMultipart()
+
+		state, err := lib.RandomStr(10)
+		require.NoError(t, err)
+
+		form.Insert("scope", "openid profile email")
+		form.Insert("response_type", "code")
+		form.Insert("client_id", clientId)
+		form.Insert("redirect_uri", "https://example.test")
+		form.Insert("state", state)
+		form.Insert("response_mode", "query")
+		form.Insert("nonce", "nonce_test")
+		form.Insert("display", "page")
+		form.Insert("prompt", "login consent")
+		form.Insert("max_age", "3600")
+		form.Insert("ui_locales", "ja_JP")
+		form.Insert("id_token_hint", "id_token_hint_test")
+		form.Insert("login_hint", "login_hint_test")
+		form.Insert("acr_values", "acr_values_test")
+
+		m, err := easy.NewFormData("/", "POST", form)
+		require.NoError(t, err)
+		m.Cookie(cookies)
+
+		c := m.Echo()
+
+		err = h.OIDCLoginHandler(c)
+		require.EqualError(t, err, "code=400, error=invalid_request_uri, message=user is not allowed")
+	})
 }
 
-// TODO: テスト
 func TestOIDCCancelHandler(t *testing.T) {
+	ctx := context.Background()
+	h := NewTestHandler(t)
 
+	SessionTest(t, h.OIDCCancelHandler, func(ctx context.Context, u *models.User) *easy.MockHandler {
+		clientId, _ := RegisterClient(t, ctx, u, "openid", "profile")
+
+		r := models.ClientRedirect{
+			ClientID: clientId,
+			URL:      "https://example.test",
+			Host:     "example.test",
+		}
+		err := r.Insert(ctx, DB, boil.Infer())
+		require.NoError(t, err)
+
+		form := easy.NewMultipart()
+
+		state, err := lib.RandomStr(10)
+		require.NoError(t, err)
+
+		form.Insert("scope", "openid profile email")
+		form.Insert("response_type", "code")
+		form.Insert("client_id", clientId)
+		form.Insert("redirect_uri", "https://example.test")
+		form.Insert("state", state)
+		form.Insert("response_mode", "query")
+		form.Insert("nonce", "nonce_test")
+		form.Insert("display", "page")
+		form.Insert("prompt", "login consent")
+		form.Insert("max_age", "3600")
+		form.Insert("ui_locales", "ja_JP")
+		form.Insert("id_token_hint", "id_token_hint_test")
+		form.Insert("login_hint", "login_hint_test")
+		form.Insert("acr_values", "acr_values_test")
+
+		m, err := easy.NewFormData("/", "POST", form)
+		require.NoError(t, err)
+		return m
+	})
+
+	t.Run("成功: キャンセルできる", func(t *testing.T) {
+		email := RandomEmail(t)
+		u := RegisterUser(t, ctx, email)
+		clientId, _ := RegisterClient(t, ctx, &u, "openid", "profile")
+
+		r := models.ClientRedirect{
+			ClientID: clientId,
+			URL:      "https://example.test",
+			Host:     "example.test",
+		}
+		err := r.Insert(ctx, DB, boil.Infer())
+		require.NoError(t, err)
+
+		cookies := RegisterSession(t, ctx, &u)
+
+		form := easy.NewMultipart()
+
+		state, err := lib.RandomStr(10)
+		require.NoError(t, err)
+
+		form.Insert("scope", "openid profile email")
+		form.Insert("response_type", "code")
+		form.Insert("client_id", clientId)
+		form.Insert("redirect_uri", "https://example.test")
+		form.Insert("state", state)
+		form.Insert("response_mode", "query")
+		form.Insert("nonce", "nonce_test")
+		form.Insert("display", "page")
+		form.Insert("prompt", "login consent")
+		form.Insert("max_age", "3600")
+		form.Insert("ui_locales", "ja_JP")
+		form.Insert("id_token_hint", "id_token_hint_test")
+		form.Insert("login_hint", "login_hint_test")
+		form.Insert("acr_values", "acr_values_test")
+
+		m, err := easy.NewFormData("/", "POST", form)
+		require.NoError(t, err)
+		m.Cookie(cookies)
+
+		c := m.Echo()
+
+		err = h.OIDCCancelHandler(c)
+		require.NoError(t, err)
+
+		response := src.OauthResponse{}
+		require.NoError(t, m.Json(&response))
+
+		require.NotEmpty(t, response.RedirectUrl)
+
+		redirectUrl, err := url.Parse(response.RedirectUrl)
+		require.NoError(t, err)
+
+		require.Equal(t, redirectUrl.Host, "example.test")
+		require.Equal(t, redirectUrl.Query().Get("state"), state, "stateが一致する")
+
+		require.Empty(t, redirectUrl.Query().Get("code"), "codeがない")
+		require.Equal(t, redirectUrl.Query().Get("error"), "access_denied", "errorがaccess_denied")
+	})
 }
