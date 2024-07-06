a88c59d66e2ff1408106c8e50b939c1d6174648a
cateiru <yuto.w51942@gmail.com>
2024-07-06 22:28:23 +0900

    fix

--- components/Client/ClientListTable.tsx
+++ components/Client/ClientListTable.tsx
@@ -1,24 +1,8 @@
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  Skeleton,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import {Box, Flex, Text, useColorModeValue} from '@chakra-ui/react';
 import React from 'react';
 import type {ClientList as ClientListType} from '../../utils/types/client';
 import {Avatar} from '../Common/Chakra/Avatar';
 import {Link} from '../Common/Next/Link';
-import {AgoTime} from '../Common/Time';
 import {useSecondaryColor} from '../Common/useColor';
 
 interface Props {
@@ -74,81 +58,4 @@ export const ClientListTable: React.FC<Props> = ({clients}) => {
       })}
     </Box>
   );
-
-  // return (
-  //   <TableContainer>
-  //     <Table variant="simple">
-  //       <Thead>
-  //         <Tr>
-  //           <Th></Th>
-  //           <Th>クライアント名</Th>
-  //           <Th>説明</Th>
-  //           <Th>作成日</Th>
-  //           <Th></Th>
-  //         </Tr>
-  //       </Thead>
-  //       <Tbody>
-  //         {clients
-  //           ? clients.map(v => {
-  //               return (
-  //                 <Tr key={`client-list-${v.client_id}`}>
-  //                   <Td>
-  //                     <Center>
-  //                       <Avatar src={v.image ?? ''} size="sm" />
-  //                     </Center>
-  //                   </Td>
-  //                   <Td>{v.name}</Td>
-  //                   <Td
-  //                     maxW="200px"
-  //                     textOverflow="ellipsis"
-  //                     whiteSpace="nowrap"
-  //                     overflowX="hidden"
-  //                   >
-  //                     {v.description}
-  //                   </Td>
-  //                   <Td>
-  //                     <AgoTime time={v.created_at} />
-  //                   </Td>
-  //                   <Td>
-  //                     <Button
-  //                       size="sm"
-  //                       colorScheme="cateiru"
-  //                       as={Link}
-  //                       href={`/client/${v.client_id}`}
-  //                     >
-  //                       詳細
-  //                     </Button>
-  //                   </Td>
-  //                 </Tr>
-  //               );
-  //             })
-  //           : Array(5)
-  //               .fill(0)
-  //               .map((_, i) => {
-  //                 return (
-  //                   <Tr key={`loading-client-${i}`}>
-  //                     <Td>
-  //                       <Center>
-  //                         <Skeleton w="32px" h="32px" borderRadius="50%" />
-  //                       </Center>
-  //                     </Td>
-  //                     <Td>
-  //                       <Skeleton w="100px" h="16px" />
-  //                     </Td>
-  //                     <Td>
-  //                       <Skeleton w="200px" h="16px" />
-  //                     </Td>
-  //                     <Td>
-  //                       <Skeleton w="100px" h="16px" />
-  //                     </Td>
-  //                     <Td>
-  //                       <Skeleton w="52px" h="32px" borderRadius="0.375rem" />
-  //                     </Td>
-  //                   </Tr>
-  //                 );
-  //               })}
-  //       </Tbody>
-  //     </Table>
-  //   </TableContainer>
-  // );
 };
